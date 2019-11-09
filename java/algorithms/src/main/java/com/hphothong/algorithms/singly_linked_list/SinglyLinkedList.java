9aadb23d2753b362586c9d3877121a233500d05d
hphothong <hphothong@gmail.com>
2019-11-08 18:11:07 -0700

    Adding singly linked list to the project

---
+++ java/algorithms/src/main/java/com/hphothong/algorithms/singly_linked_list/SinglyLinkedList.java
@@ -0,0 +1,11 @@
+package com.hphothong.algorithms.singly_linked_list;
+
+import java.util.Collection;
+
+public class SinglyLinkedList<T> {
+    private Collection<LinkedListNode<T>> list;
+
+    public SinglyLinkedList() {
+
+    }
+}
